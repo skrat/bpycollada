2115381909ee1fe96890688879fe6cd51aaa693e
Dusan Maliarik <dusan.maliarik@gmail.com>
2011-11-27 13:23:32 +0100

    triangle mesh export

--- export_collada.py
+++ export_collada.py
@@ -3,9 +3,11 @@ import numpy as np
 from mathutils import Matrix, Vector
 
 from collada import Collada
+from collada.geometry import Geometry
 from collada.scene import Node, Scene
 from collada.scene import GeometryNode
 from collada.scene import MatrixTransform
+from collada.source import FloatSource, InputList
 
 
 def save(op, context,
@@ -28,6 +30,7 @@ class ColladaExport(object):
     def __init__(self, directory, export_as='dae_only'):
         self._dir = directory
         self._export_as = export_as
+        self._geometries = {}
         self._collada = Collada()
 
         self._scene = Scene('main', [])
@@ -68,12 +71,45 @@ class ColladaExport(object):
         node.save()
         return node
 
-    # def obj_MESH(self, b_obj):
-    #     geom = ?
-    #     return GeometryNode(geom, [])
+    def obj_MESH(self, b_obj):
+        geom = self._geometries.get(b_obj.data.name, None)
+        if not geom:
+            geom = self.mesh(b_obj.data)
+            self._geometries[b_obj.data.name] = geom
+        return GeometryNode(geom, [])
+
+    def mesh(self, b_mesh):
+        vert_srcid = b_mesh.name + '-vertary'
+        vert_f = [c for v in b_mesh.vertices for c in v.co]
+        vert_src = FloatSource(vert_srcid, np.array(vert_f), ('X', 'Y', 'Z'))
+
+        norm_srcid = b_mesh.name + '-normary'
+        norm_f = [c for v in b_mesh.vertices for c in v.normal]
+        norm_src = FloatSource(norm_srcid, np.array(norm_f), ('X', 'Y', 'Z'))
+
+        name = b_mesh.name + '-geom'
+        geom = Geometry(self._collada, name, name, [vert_src, norm_src])
+        ilist = InputList()
+        ilist.addInput(0, 'VERTEX', _url(vert_srcid))
+        ilist.addInput(1, 'NORMAL', _url(norm_srcid))
+        indices = np.array([i for v in [
+            (v, v) for f in b_mesh.faces for v in f.vertices]
+            for i in v])
+        triset = geom.createTriangleSet(indices, ilist, 'none')
+
+        geom.primitives.append(triset)
+        self._collada.geometries.append(geom)
+        return geom
 
     def matrix(self, b_matrix):
         f = tuple(map(tuple, b_matrix.transposed()))
         return MatrixTransform(np.array(
             [e for r in f for e in r], dtype=np.float32))
+
+
+def _is_trimesh(b_mesh):
+    return all([len(f.vertices) == 3 for f in b_mesh.faces])
+
+def _url(uid):
+    return '#' + uid
 
